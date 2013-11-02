2e0eb14430ccbe6b99847c3f5419bc21c1b6c3b4
Fran <Fran@Family>
2013-11-02 15:08:04 -0300

    Redefined remove and getAll

--- src/frd/persistence/MovementManager.java
+++ src/frd/persistence/MovementManager.java
@@ -9,7 +9,18 @@ public class MovementManager extends DataManager {
 
 	public static List<Movement> getAll() {
 		//TODO: devolver todos los movimientos persistidos en el datastore
-		return new ArrayList<Movement>();
+		return DataManager.getAll(Movement.class);			
 	}
-
+	
+	public static boolean remove( Long id ){
+		Movement m = pm.getObjectById( Movement.class, id );
+		if ( m != null ) {
+			DataManager.remove( m );
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
 }
