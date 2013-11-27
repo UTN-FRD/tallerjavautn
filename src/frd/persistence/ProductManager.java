96a913207acf8119a33dedac1395345c420fee7e
LucasJC <lucas.couchot@gmail.com>
2013-11-27 20:41:52 -0300

    método getAllExpired para ProductManager

--- src/frd/persistence/ProductManager.java
+++ src/frd/persistence/ProductManager.java
@@ -1,7 +1,14 @@
 package frd.persistence;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
 import java.util.List;
 
+import javax.jdo.PersistenceManager;
+import javax.jdo.Query;
+
+import frd.model.Lot;
 import frd.model.Product;
 
 public class ProductManager extends DataManager {
@@ -9,6 +16,47 @@ public class ProductManager extends DataManager {
 	public static List<Product> getAll() {
 		return DataManager.getAll(Product.class);
 		//return new ArrayList<Product>();
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static List<Product> getAllExpired(){
+		
+		List<Product> ret = new ArrayList<Product>();
+		List<Lot> results = null;
+		PersistenceManager pm = PMF.get().getPersistenceManager();
+		
+		Query query = pm.newQuery(Lot.class);
+		query.declareImports("import java.util.Date");
+		query.declareParameters("Long productId, Date hoy");
+		query.setFilter("product == productId & dueDate < hoy");
+		
+		Date date = new Date();
+		Long productId = null;
+		
+		for(Product producto : ProductManager.getAll()){
+			
+			try{
+				
+				productId = producto.getId();
+				
+				results = (List<Lot>) query.execute(productId, date);
+				
+			}catch(Exception ex){
+				
+				ex.printStackTrace();
+			}
+			
+			if(results.size() > 0){
+				
+				ret.add(producto);
+			}
+			
+		}
+		
+		query.closeAll();
+	
+		return ret;
+		
 	}
 
 }
